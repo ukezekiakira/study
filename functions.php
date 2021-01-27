769a64b858f9297c1ad2dc3706dcd934246f7e0e
akira ukezeki <aukezeki9@gmail.com>
2021-01-27 22:05:16 +0900

    アクションフックｃｓｓ、サムネイル設定

--- functions.php
+++ functions.php
@@ -1,1 +1,15 @@
-<?php add_theme_support('post-thumbnails'); 
+<?php 
+    function sakura_theme_setup() {
+    add_theme_support('post-thumbnails'); 
+    }
+
+        add_action("after_setup_theme","sakura_theme_setup");
+
+    
+    function sakura_theme_link() {
+
+        wp_enqueue_style("pc-css",get_template_directory_uri() . "/./css/style_pc.css");
+        wp_enqueue_style("common",get_template_directory_uri() . "/js/common.js",array(),"1.0.0",true);
+
+    }
+add_action("wp_enqueue_scripts","sakura_theme_link");
