92fc1c51a055db110baa6630b00f96bb8984bd65
akira ukezeki <aukezeki9@gmail.com>
2021-01-28 12:15:04 +0900

    functions.phpで一元管理

--- functions.php
+++ functions.php
@@ -11,9 +11,13 @@
 
         wp_enqueue_style("pc-css",get_template_directory_uri() . "/./css/style_pc.css",array(),"1.0.0","screen and ( min-width:769px )" );
         wp_enqueue_style("sp-css",get_template_directory_uri() . "/./css/style_sp.css",array(),"1.0.0","screen and ( max-width:768px )");
+        wp_enqueue_style("drawer","https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/css/drawer.min.css");
 
 
-        wp_enqueue_script("common",get_template_directory_uri() . "/js/common.js",array(),"1.0.0",true);
+        wp_enqueue_script("jquery",get_template_directory_uri() . "js/jquery-3.2.1.min.js",array(),"1.0.0",true);
+        wp_enqueue_script("iscroll", "https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.2.0/iscroll.min.js",array("jQuery"),"1.0.0",true);
+        wp_enqueue_script("drawer", "https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/js/drawer.min.js",array("jQuery","iscroll"),"1.0.0",true);
+        wp_enqueue_script("common",get_template_directory_uri() . "/js/common.js",array"jQuery","iscroll","drawer"),"1.0.0",true);
 
     }
 add_action("wp_enqueue_scripts","sakura_theme_link");
--- header.php
+++ header.php
@@ -14,18 +14,18 @@
     <!-- <link rel="stylesheet" type="text/css" media="screen and ( min-width:769px )" href="<?php echo get_template_directory_uri(); ?>/./css/style_pc.css">
 	<link rel="stylesheet" type="text/css" media="screen and ( max-width:768px )" href="<?php echo get_template_directory_uri(); ?>/./css/style_sp.css"> -->
 
-    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <!-- <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script> -->
 <!-- drawer.css -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/css/drawer.min.css">
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/css/drawer.min.css"> -->
 <!-- jquery & iScroll -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.2.0/iscroll.min.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.2.0/iscroll.min.js"></script> -->
 <!-- drawer.js -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/js/drawer.min.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/js/drawer.min.js"></script> -->
 <!-- <script type="text/javascript" src="js/jquery.fadethis.min.js"></script> -->
 
 <!-- <script type="text/javascript" src="js/common.js"></script> -->
-<link rel="stylesheet" type="text/css" media="screen and ( min-width:769px )" href="./css/style_pc.css">
-<link rel="stylesheet" type="text/css" media="screen and ( max-width:768px )" href="./css/style_sp.css">
+<!-- <link rel="stylesheet" type="text/css" media="screen and ( min-width:769px )" href="./css/style_pc.css">
+<link rel="stylesheet" type="text/css" media="screen and ( max-width:768px )" href="./css/style_sp.css"> -->
 <?php wp_head(); ?>
     </head>
 
